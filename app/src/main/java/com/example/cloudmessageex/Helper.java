a225ab1a835c5df8845babcb6b44ac5520ed2d4d
KyleCagayan <43043797+KyleCagayan@users.noreply.github.com>
2022-05-17 10:41:42 -0400

    pushed whole app

---
+++ app/src/main/java/com/example/cloudmessageex/Helper.java
@@ -0,0 +1,20 @@
+package com.example.cloudmessageex;
+
+public class Helper {
+    private String url="https://api.paxstores.com/p-market-api/v1";
+
+    private String appKey="OGFG33G65KOVGPUWSYNC";
+    private String appSecret="YQV4ZIMUVDWNFZV1I4E7RO6S8D5CI44NGKZKGOWC";
+
+    public String getUrl(){
+        return url;
+    }
+
+    public String getAppKey(){
+        return appKey;
+    }
+
+    public String getAppSecret(){
+        return appSecret;
+    }
+}
